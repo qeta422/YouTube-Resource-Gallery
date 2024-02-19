9426883e160645e63b9c2de32d52ac324a96b342
qeta422 <qetafetviashvili123@gmail.com>
2024-02-19 23:59:52 +0400

    form to save video id-s to the local storage

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 const videoContainer = document.getElementById('videoContainer');
-
+const videoIdInput = document.getElementById('videoID');
 let youTubeVideoIds = [];
 
 const loadVideos = () => {
     youTubeVideoIds = JSON.parse(localStorage.getItem('youTubeVideoIds')) || [];
-    console.log(youTubeVideoIds);
-}
+};
 
 const displayVideos = () => {
-    const videosHTMLString = youTubeVideoIds.map( id => `
+    const videosHTMLString = youTubeVideoIds.map( (id) => `
         <li onclick="clickVideo(event, '${id}'">
         <img class="thumbnail" src="https://i3.ytimg.com/vi/${id}/sddefault.jpg" 
         alt= "Cover image for YouTube video with id ${id}">
@@ -16,13 +15,16 @@ const displayVideos = () => {
         </li>
     `).join("");
     videoContainer.innerHTML = videosHTMLString;
-    console.log(videosHTMLString);
+};
 
-    // for (let i = 0; i < youTubeVideoIds.length; i++) {
-    //     console.log(youTubeVideoIds[i]);
-        
-    // }
-}
+const saveVideo = (e) => {
+    e.preventDefault();
+    const videoId = videoIdInput.value;
+    youTubeVideoIds.unshift(videoId);
+    videoIdInput.value = "";
+    localStorage.setItem('youTubeVideoIds', JSON.stringify(youTubeVideoIds));
+    displayVideos();
+};
 
 loadVideos();
 displayVideos();
