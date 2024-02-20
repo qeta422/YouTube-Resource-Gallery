a4620fd8935d762750477b6daa5d590953fd790a
qeta422 <qetafetviashvili123@gmail.com>
2024-02-20 23:56:53 +0400

    handling delete

--- js/main.js
+++ js/main.js
@@ -8,20 +8,30 @@ const loadVideos = () => {
 
 const displayVideos = () => {
     const videosHTMLString = youTubeVideoIds.map( (id) => `
-        <li onclick="clickVideo(event, '${id}'">
+        <li onclick="clickVideo(event, '${id}')">
         <img class="thumbnail" src="https://i3.ytimg.com/vi/${id}/sddefault.jpg" 
         alt= "Cover image for YouTube video with id ${id}">
         <button class="delete-btn">&times;</button>
         </li>
-    `).join("");
+    `).join('');
     videoContainer.innerHTML = videosHTMLString;
+};
+
+const clickVideo = (event, id) => {
+    console.log(event, id);
+    if (event.target.classList.contains("delete-btn")) {
+        youTubeVideoIds = youTubeVideoIds.filter(i => i != id);
+        console.log(youTubeVideoIds);
+        localStorage.setItem('youtubeVideoIds', JSON.stringify(youTubeVideoIds));
+        displayVideos();
+    }
 };
 
 const saveVideo = (e) => {
     e.preventDefault();
     const videoId = videoIdInput.value;
     youTubeVideoIds.unshift(videoId);
-    videoIdInput.value = "";
+    videoIdInput.value = '';
     localStorage.setItem('youTubeVideoIds', JSON.stringify(youTubeVideoIds));
     displayVideos();
 };
